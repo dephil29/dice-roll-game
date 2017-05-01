073474a3a7ff1c93235b543a170dd9092ce3ff04
Eric Phillips <Eric Phillips>
2017-05-01 11:25:01 -0400

    first commit

---
+++ js/setup.js
@@ -0,0 +1,10 @@
+var die = document.querySelector('#die');
+var roll = document.querySelector('#roll');
+var player1 = document.querySelector('#player1');
+var player2 = document.querySelector('#player2');
+var tiles = document.querySelectorAll('.tile');
+
+// game state
+var player1Position = 0;
+var player2Position = 0;
+var turn = player1;
